e7250ab494827bc308d1feb6ca03e4fbbb7c7bbf
aiden <kakao@MacBook-Pro-9.local>
2021-03-31 23:38:02 +0900

    a

--- js/info.js
+++ js/info.js
@@ -1,31 +1,31 @@
 var projectInfo = [
   {
-    title: 'Product Design : Hey Kakao',
+    title: 'Product Design :<br> Hey Kakao',
     subtitle: 'UI / UX Design<br>Interaction Design',
     date: '2020',
-    titleImg: 'img/Concept3_Perspective.jpg',
+    titleImg: 'img/content_heykakao/main_img.png',
     linkPage: 'content_heykakao.html'
   },
   {
     title: 'kakao i Service : Visual Components Design and Guide',
     subtitle: 'UI / UX Design<br>Interaction Design',
     date: '2018',
-    titleImg: 'img/Concept1_Perspective.jpg',
+    titleImg: 'img/content_01/main.png',
     linkPage: 'content_kakaoi.html'
   },
   {
-    title: 'Side Project for Studying javascript : My Awesome Film',
-    subtitle: 'UI / UX Design<br>Interaction Design',
-    date: '2019',
-    titleImg: 'img/Concept0_Perspective.jpg',
-    linkPage: 'content_00.html'
-  },
-  {
-    title: 'Service Design for Smart Display : Kakao mini',
+    title: 'Shoescan : AI Driven Visual Search Commerce App',
     subtitle: 'UI / UX Design<br>Interaction Design',
     date: '2017',
-    titleImg: 'img/Concept2_Perspective.jpg',
-    linkPage: 'content_02.html'
+    titleImg: 'https://cathy03.github.io/portfolio/images/sc/sc_title.png',
+    linkPage: 'https://cathy03.github.io/portfolio/04_shoescan.html'
+  },
+  {
+    title: 'Project Anne : Fashion Streaming Service',
+    subtitle: 'UI Design<br>Interaction Design',
+    date: '2016',
+    titleImg: 'https://cathy03.github.io/portfolio/images/pa/pa_main_bg.jpg',
+    linkPage: 'https://cathy03.github.io/portfolio/03_project_anne.html'
   },
 
 
